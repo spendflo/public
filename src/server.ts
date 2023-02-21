49e5b8dc338906c823706752f998e36d901a87ad
Ajay Vardhan <ajay@spendflo.com>
2023-02-21 12:34:21 +0530

    initial server setup

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/.well-known/microsoft-identity-association.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.sendFile(path.join(__dirname, 'public', '.well-known', 'microsoft-identity-association.json'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
