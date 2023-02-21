31e00f7309d14e92bf4e42c1a0a2479af805de74
Ajay Vardhan <ajay@spendflo.com>
2023-02-21 16:26:28 +0530

    move from public to root

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import path from 'path';
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/.well-known/microsoft-identity-association.json', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
-    res.sendFile(path.join(__dirname, 'public', '.well-known', 'microsoft-identity-association.json'));
+    res.sendFile(path.join(__dirname, '.well-known', 'microsoft-identity-association.json'));
 });
 
 const port = process.env.PORT || 3000;
